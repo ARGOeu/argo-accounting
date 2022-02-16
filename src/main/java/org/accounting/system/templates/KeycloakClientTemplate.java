bc0c7cdd0dd503396f16741f1714d3032bd7e96d
fbasios <fbasios@admin.grnet.gr>
2022-02-16 15:24:54 +0200

    Authentication Process
    The Accounting System API resources have been protected by an external keycloak.
    What that means practically, is that these resources  can only be accessed if a client is sending
    a bearer token along with the request, which must be valid and trusted by the Accounting System API.
    
    There also is a UI Page at {accounting_system_host} which is responsible for :
    
    * redirecting a user to Keycloak's login page in order to be authenticated
    * displaying the obtained token
    
    [ACC-24]

---
+++ src/main/java/org/accounting/system/templates/KeycloakClientTemplate.java
@@ -0,0 +1,38 @@
+package org.accounting.system.templates;
+
+import io.quarkus.qute.Template;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+
+import javax.inject.Inject;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+/**
+ * This endpoint is responsible for rendering the keycloak.html.
+ * The web page is created by processing template file. Templates are located under src/main/resources/templates.
+ * Obtain the keycloak application properties and feed them to the keycloak template.
+ * The keycloak.html is responsible for redirecting a user to Keycloak's login page in order to be authenticated and displaying the obtained token.
+ */
+@Path("/keycloak")
+public class KeycloakClientTemplate {
+
+    @Inject
+    Template keycloak;
+
+    @ConfigProperty(name = "keycloak.server.url")
+    String keycloakServerUrl;
+
+    @ConfigProperty(name = "keycloak.server.realm")
+    String keycloakServerRealm;
+
+    @ConfigProperty(name = "keycloak.server.client.id")
+    String keycloakServerClientId;
+
+    @GET
+    @Produces(MediaType.TEXT_HTML)
+    public String keycloakClient() {
+        return keycloak.data("keycloak_server_url", keycloakServerUrl, "keycloak_server_realm", keycloakServerRealm, "keycloak_server_client_id", keycloakServerClientId).render();
+    }
+}
