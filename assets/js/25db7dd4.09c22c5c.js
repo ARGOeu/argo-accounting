9aaeff447049586a1e1ff991c5bb446764a221f4
newgrnetci <argo@grnet.gr>
2022-09-02 23:54:56 +0000

    Deploy website - based on 627f27c271df9a3cad9db17053f6509bba07792b

--- assets/js/25db7dd4.09c22c5c.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkaccounting_system=self.webpackChunkaccounting_system||[]).push([[549],{5745:function(s){s.exports=JSON.parse('{"name":"docusaurus-plugin-content-pages","id":"default"}')}}]);
