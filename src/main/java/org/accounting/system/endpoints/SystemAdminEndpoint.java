890039f7213f0f0f922ff2d76436afb842f1041a
fbasios <fbasios@admin.grnet.gr>
2024-04-16 10:55:00 +0300

    Update docusaurus document

--- src/main/java/org/accounting/system/endpoints/SystemAdminEndpoint.java
+++ src/main/java/org/accounting/system/endpoints/SystemAdminEndpoint.java
@@ -7,17 +7,34 @@ import jakarta.validation.constraints.NotNull;
 import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
+import org.accounting.system.dtos.InformativeResponse;
+import org.accounting.system.dtos.admin.ProjectRegistrationRequest;
 import org.accounting.system.interceptors.annotations.SystemAdmin;
 import org.accounting.system.services.SystemAdminService;
 import org.accounting.system.util.Utility;
 import org.eclipse.microprofile.openapi.annotations.Operation;
-
-import java.util.Set;
+import org.eclipse.microprofile.openapi.annotations.enums.SchemaType;
+import org.eclipse.microprofile.openapi.annotations.enums.SecuritySchemeIn;
+import org.eclipse.microprofile.openapi.annotations.enums.SecuritySchemeType;
+import org.eclipse.microprofile.openapi.annotations.media.Content;
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
+import org.eclipse.microprofile.openapi.annotations.security.SecurityRequirement;
+import org.eclipse.microprofile.openapi.annotations.security.SecurityScheme;
+import org.eclipse.microprofile.openapi.annotations.tags.Tag;
 
 @Path("/system-admin")
 @Authenticated
+@SecurityScheme(securitySchemeName = "Authentication",
+        description = "JWT token",
+        type = SecuritySchemeType.HTTP,
+        scheme = "bearer",
+        bearerFormat = "JWT",
+        in = SecuritySchemeIn.HEADER)
+
 public class SystemAdminEndpoint {
 
     @Inject
@@ -26,16 +43,51 @@ public class SystemAdminEndpoint {
     @Inject
     Utility utility;
 
+    @Tag(name = "System Administrator")
     @Operation(
-            hidden = true)
+            summary = "Endpoint for registering Projects into the Accounting Service.",
+            description = "This endpoint allows system administrators to register a new project into the Accounting Service. Access to this endpoint is restricted to system administrators only.")
+    @APIResponse(
+            responseCode = "200",
+            description = "Informs about the success or failure of the registration process.",
+            content = @Content(schema = @Schema(
+                    type = SchemaType.OBJECT,
+                    implementation = InformativeResponse.class)))
+    @APIResponse(
+            responseCode = "400",
+            description = "Bad Request.",
+            content = @Content(schema = @Schema(
+                    type = SchemaType.OBJECT,
+                    implementation = InformativeResponse.class)))
+    @APIResponse(
+            responseCode = "401",
+            description = "Client has not been authenticated.",
+            content = @Content(schema = @Schema(
+                    type = SchemaType.OBJECT,
+                    implementation = InformativeResponse.class)))
+    @APIResponse(
+            responseCode = "403",
+            description = "The authenticated client is not permitted to perform the requested operation.",
+            content = @Content(schema = @Schema(
+                    type = SchemaType.OBJECT,
+                    implementation = InformativeResponse.class)))
+    @APIResponse(
+            responseCode = "500",
+            description = "Internal Server Errors.",
+            content = @Content(schema = @Schema(
+                    type = SchemaType.OBJECT,
+                    implementation = InformativeResponse.class)))
+    @SecurityRequirement(name = "Authentication")
+
     @POST
-    @Path("/assign-projects")
+    @Path("/register-projects")
+    @Produces(value = MediaType.APPLICATION_JSON)
     @Consumes(value = MediaType.APPLICATION_JSON)
     @SystemAdmin
-    public Response assignProjects(@Valid @NotNull(message = "The request body is empty.") Set<String> projects) {
+    public Response assignProjects(@Valid @NotNull(message = "The request body is empty." ) ProjectRegistrationRequest request) {
 
-        systemAdminService.registerProjectsToAccountingService(projects, utility.getClientVopersonId());
+        var response = systemAdminService.registerProjectsToAccountingService(request.projects, utility.getClientVopersonId());
 
-        return Response.ok().build();
+        return Response.ok().entity(response).build();
     }
 }
